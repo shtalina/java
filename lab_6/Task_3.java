5e674890b09c5ba0280a54430ddf6226b4082169
shtalina <99807081+shtalina@users.noreply.github.com>
2022-12-13 22:02:22 +0700

    Update Task_3.java

--- lab_6/Task_3.java
+++ lab_6/Task_3.java
@@ -7,12 +7,23 @@ public class Task_3 {
     public static void main(String[] args) {
         final int months_per_year = 12;
 
-        double annual_interest_rate, monthly_interest_rate, loan_amount, monthly_payment, total_payment;
+        double annual_interest_rate = 0, monthly_interest_rate, loan_amount, monthly_payment, total_payment;
         int number_of_years;
 
         Scanner input = new Scanner(System.in);
-        System.out.println("ставка по кредиту: ");
-        annual_interest_rate = input.nextDouble();
+        System.out.println("ставка по кредиту (1-ипотека; 2-авто; 3-обрразование): ");
+        int stavka = input.nextInt();
+        switch (stavka) {
+            case 1:
+                annual_interest_rate = 4.5;
+                break;
+            case 2:
+                annual_interest_rate = 6.2;
+                break;
+            case 3:
+                annual_interest_rate = 7.5;
+                break;
+        }
 
         System.out.println("период кредитования(года): ");
         number_of_years = input.nextInt();
@@ -22,12 +33,12 @@ public class Task_3 {
 
         monthly_interest_rate = annual_interest_rate / 12 / 100;
 
-        monthly_payment = loan_amount * monthly_interest_rate / (1 - 1 / Math.pow(1 + monthly_interest_rate, number_of_years * months_per_year));
+        monthly_payment = loan_amount * monthly_interest_rate
+                / (1 - 1 / Math.pow(1 + monthly_interest_rate, number_of_years * months_per_year));
         total_payment = monthly_payment * number_of_years * months_per_year;
 
-        System.out.println("ежемесячный платеж: " + (int)(monthly_payment * 100) / 100.0 + "руб.");
-        System.out.println("стоимость кредита: " + (int)(total_payment * 100) / 100.0 + "руб.");
-
+        System.out.println("ежемесячный платеж: " + (int) (monthly_payment * 100) / 100.0 + "руб.");
+        System.out.println("стоимость кредита: " + (int) (total_payment * 100) / 100.0 + "руб.");
 
     }
 
